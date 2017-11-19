94554fcf81639450bf145f717bd730003ca9b5c4
owen <thss15_zhangqr@163.com>
2017-11-19 12:17:45 +0800

    add answer to first two problems

---
+++ answer.sh
@@ -0,0 +1,7 @@
+#bin/bash
+# ./hex2raw < answer1.txt > answer1-raw.txt
+# ./bufbomb -u 2015013226 < answer1-raw.txt
+# ./hex2raw < answer2.txt > answer2-raw.txt
+# ./bufbomb -u 2015013226 < answer2-raw.txt
+./hex2raw < answer3.txt > answer3-raw.txt
+./bufbomb -u 2015013226 < answer3-raw.txt
