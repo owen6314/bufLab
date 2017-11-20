8d15d011b67c7111c2d658d47bf2b9adcbb096cd
owen <thss15_zhangqr@163.com>
2017-11-20 15:05:32 +0800

    add answer4

--- answer.sh
+++ answer.sh
@@ -3,5 +3,9 @@
 # ./bufbomb -u 2015013226 < answer1-raw.txt
 # ./hex2raw < answer2.txt > answer2-raw.txt
 # ./bufbomb -u 2015013226 < answer2-raw.txt
-./hex2raw < answer3.txt > answer3-raw.txt
-./bufbomb -u 2015013226 < answer3-raw.txt
+# ./hex2raw < answer3.txt > answer3-raw.txt
+# ./bufbomb -u 2015013226 < answer3-raw.txt
+# ./hex2raw < answer4.txt > answer4-raw.txt
+# ./bufbomb -u 2015013226 < answer4-raw.txt
+./hex2raw -n < answer5.txt > answer5-raw.txt
+./bufbomb -n -u 2015013226 < answer5-raw.txt
